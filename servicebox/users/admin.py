2795b2e4b1c72525492216ad59adb39c8debcc04
Amadeusz Pawlik <amadeusz@nostromo.lan>
2018-10-31 20:18:18 +0100

    Cleanup trash and commented code

--- servicebox/users/admin.py
+++ servicebox/users/admin.py
@@ -4,28 +4,4 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-#class UserCreateForm(UserCreationForm):
-#
-#    class Meta:
-#        model = User
-#        fields = ('email', 'first_name' , 'last_name', 'company' )
-#
-#class CustomUserAdmin(UserAdmin):
-#    add_form = UserCreateForm
-#
-#    UserAdmin.fieldsets += (
-#        (None, {
-#            'classes': ('wide',),
-#            'fields': ('company', 'role'),
-#        }),
-#    )
-#
-#    UserAdmin.add_fieldsets += (
-#        (None, {
-#            'classes': ('wide',),
-#            'fields': ('company', 'role'),
-#        }),
-#    )
-#    UserAdmin.list_display += ('company', 'role')
-#
 admin.site.register(User)
